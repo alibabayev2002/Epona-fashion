87d7d1a7d7935ab420619e34bb14dc2a85a328c4
alibabayev2002 <acemiyt23@gmail.com>
2019-11-01 10:52:21 +0400

    css

--- home.php
+++ home.php
@@ -1,9 +1,18 @@
 <div class="flex">
         <div style='flex:1;'>
-                <p>  Ən yaxşıları bizdə..</p>
+                <p style="font:arial;font-weight:none;font-size:20px;">  Ən yaxşıları bizdə..</p>
+                <button class="shop_button" style="padding:2% 5%; background-color:white;border:1px solid; margin-top:2%;">SHOP NOW</button>
         </div>
         <div style="flex:1;">
-                <img src="wallpaper3.jpg" width="450px" alt=""> saa
+                <img src="wallpaper3.jpg" width="450px" alt=""> 
         </div>
+        
 </div>
+<!-- <div class="flex" style='display:flex;'>
+                <p style="font:arial;font-weight:none;font-size:20px;">  Ən yaxşıları bizdə..</p>
+                <button>SHOP NOW</button>
+        </div>
+        <div style="flex:1;">
+                <img src="wallpaper3.jpg" width="450px" alt=""> 
+        </div> -->
 <!-- OUTDOOR ADVENTURE, URBAN EXPLORATION, AND INNOVATIVE DESIGN -->
