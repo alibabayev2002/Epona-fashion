580451cc17d8ac98782dffbd80966a5a8ae1cb19
alibabayev2002 <acemiyt23@gmail.com>
2019-11-01 19:29:04 +0400

    Some changes

--- home.php
+++ home.php
@@ -1,13 +1,46 @@
+<style>
+        img:hover {
+                opacity: .4;
+                transition: 600ms all;
+        }
+</style>
+<br><br>
 <div class="flex">
         <div style='flex:1;'>
-                <p style="font:arial;font-weight:none;font-size:20px;">  Ən yaxşıları bizdə..</p>
-                <button class="shop_button" style="padding:2% 5%; background-color:white;border:1px solid; margin-top:2%;">SHOP NOW</button>
+                <p style="font-family: 'Raleway', sans-serif;font-weight:none;font-size:20px;"> OUTDOOR ADVENTURE URBAN
+                        EXPLORATION</p>
+                <button class="shop_button" style="padding:2% 5%; border:1px solid; margin-top:2%;">SHOP NOW</button>
         </div>
         <div style="flex:1;">
-                <img src="wallpaper3.jpg" width="450px" alt=""> 
+                <img src="wallpaper.jpg" width="450px" alt="">
         </div>
-        
 </div>
+<br><br><br><br><br><br>
+<p style="font-family: 'Raleway', sans-serif;font-weight:none;font-size:20px;"> OUTDOOR ADVENTURE, URBAN EXPLORATION,
+        AND INNOVATIVE DESIGN </p>
+<br><br><br>
+<div class="flex">
+        <div style="flex:2;">
+                <img src="wallpaper3.jpg" width="470px" alt="">
+        </div>
+        <div style="flex:2;">
+
+                <img src="wallpaper2.jpg" width="470px" alt="">
+        </div>
+</div>
+<div class="flex">
+        <div style="flex:2; margin-top:15%;">
+                <img src="wallpaper4.jpg" width="61%" alt="">
+        </div>
+</div><br><br><br>
+<p style="font-family: 'Open Sans Condensed', sans-serif;font-weight:none;font-size:20px;"> ON LOCATION / JACKSON HOLE
+</p>
+<p style="font-family: 'Open Sans Condensed', sans-serif;font-size:12px;padding: 4% 19%;">Jackson Hole is a town known
+        for many things: the grandiose Teton Range, its close proximity to Yellowstone National Park and Grand Teton
+        National Park, and world-class skiing. A beloved mountain of skiers and snowboarders alike, Jackson has earned a
+        reputation for great snow, serious steeps, and imposing vertical. This winter, it is also the site of our
+        Fall/Winter 2019 shoot.</p>
+<button class="shop_button" style="padding:1% 3%; border:1px solid; ">SHOP NOW</button>
 <!-- <div class="flex" style='display:flex;'>
                 <p style="font:arial;font-weight:none;font-size:20px;">  Ən yaxşıları bizdə..</p>
                 <button>SHOP NOW</button>
